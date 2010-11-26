1eb167bee4d97b675c6f4d6495387e7725c6b5a6
Brian Michel <brian_michel@comcast.com>
2010-11-26 13:30:11 -0500

    Added in support for multiple hosts to be cholesterol'd

--- Cholesterol.rb
+++ Cholesterol.rb
@@ -5,80 +5,112 @@
 #
 require "rubygems"
 require "optparse"
-$VERSION = "0.0.2"
+$VERSION = "0.0.3"
 
+def problem?
+  File.open("problem.txt").each { |line|
+    puts line
+  }
+end
+
+def colored_message(text, color, exitcolor="0m")
+  puts "\033[#{color}#{text}\033[#{exitcolor}"
+end
 def flush_rules
    puts %x[sudo ipfw -q flush]
    puts "Okay, you should be good now!"
 end
 
-def create_rules(endpoint, delay, packetloss, connectionspeed)
-  if endpoint == nil
-    endpoint = "any"
-    puts "****Oops, no endpoint given, clogging everything.****"
-  end
-  
-  puts "Stuffing the tubes..."
-  if endpoint != "any"
-    endpointArray = %x[dig #{endpoint} +short]
-    splitArray = endpointArray.split("\n")
-    if splitArray.count > 1 #say if our host resolves to more than one IP
-      splitArray.delete_at(0)
-    end
-    count = 1
-    
-    splitArray.each do |_endpoint|
-      puts %x[sudo ipfw add pipe #{(count * 2) - 1} ip from any to #{_endpoint}]
-      puts %x[sudo ipfw add pipe #{(count * 2)} ip from #{_endpoint} to any]
-      puts %x[sudo ipfw pipe #{(count * 2) - 1} config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
-      puts %x[sudo ipfw pipe #{(count * 2)} config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
-      count += 1
-    end
-  else #if we do not pass any endpoint into the function, it only needs to create two rules to block everything.
+def create_rule(count, endpoint, delay, connectionspeed, packetloss)
+  if count != nil
+    puts %x[sudo ipfw add pipe #{(count * 2) - 1} ip from any to #{endpoint}]
+    puts %x[sudo ipfw add pipe #{(count * 2)} ip from #{endpoint} to any]
+    puts %x[sudo ipfw pipe #{(count * 2) - 1} config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
+    puts %x[sudo ipfw pipe #{(count * 2)} config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
+  else #if we do not pass any endpoint into the function, it only needs to create two rules to block everything.˝
     puts %x[sudo ipfw add pipe 1 ip from any to #{endpoint}]
     puts %x[sudo ipfw add pipe 2 ip from #{endpoint} to any]
     puts %x[sudo ipfw pipe 1 config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
     puts %x[sudo ipfw pipe 2 config delay #{delay}ms bw #{connectionspeed}MBit/s plr #{packetloss}]
-  end
-  puts "Tubes stuffed!"
+  end 
 end
 
+def create_rules(endpoint, delay, packetloss, connectionspeed)
+  if endpoint == nil #catch to see if the user has not entered an endpoint, if not, block it all!
+    endpoint = "any"
+    colored_message("****No endpoint given, clogging everything.****", "31m")
+  else
+    endpoints = endpoint.split(",")
+    endpoints.each do |r|
+      colored_message("****Creating rule for: #{r.strip}****", "31m")
+    end
+  end
+  
+  colored_message("Stuffing the tubes...", "36;44m")
+  if endpoint != "any"
+    count = 1
+    endpoints.each do |endpoint|
+      endpointArray = %x[dig #{endpoint.strip} +short]
+      splitArray = endpointArray.split("\n")
+      if splitArray.count > 1 #say if our host resolves to more than one IP
+        splitArray.delete_at(0)
+      end
+      
+      splitArray.each do |digendpoint|
+        create_rule(count, digendpoint, delay, connectionspeed, packetloss)
+        count += 1
+      end
+    end
+  else
+    create_rule(nil, endpoint, delay, connectionspeed, packetloss)
+  end
+  colored_message("Tubes stuffed!", "36;44m")
+end
+
+# Make hash and set default values
 options = {}
+options[:packetloss] = 0
+options[:delay] = 0
+options[:speed] = 100
+
 optparse = OptionParser.new do |opts|
-  opts.banner = "Usage: Cholesterol.rb -e endpoint -s speed -p packetloss -d delay"
+  opts.banner = "Usage: Cholesterol.rb -e endpoint -s speed -l packetloss -d delay"
   opts.separator ''
   opts.on('-v', '--version', 'Display Version') do
-    puts $VERSION; exit
+    puts "Cholesterol Version:" + $VERSION + " Created by Brian Michel 2010"; exit
   end
   opts.on('-i', '--interactive', "I'll guide you through setup!") do |n|
     options[:interactive] = n
   end
-  opts.on('-p', '--packet-loss (0-100)', Float, "Packet Loss Percentage" ) do |n|
+  opts.on('-l', '--packet-loss [0-100]', Float, "Packet Loss Percentage" ) do |n|
     options[:packetloss] = n/100
   end
-  opts.on('-e', '--endpoint (i.e. 10.0.0.1)', "Endpoint You Wish To Slow Up") do |n|
+  opts.on('-e', '--endpoint [IP or Hostname]', "Endpoint(s) To Slow Connectivity To (i.e. \"www.google.com, www.wikipedia.org\")") do |n|
     options[:endpoint] = n
   end
   opts.on('-f', '--flush', "Flush Old Rules") do |n|
     flush_rules
     exit
   end
-  opts.on('-d', '--delay NUM', Integer, "Delay In Milliseconds") do |n|
+  opts.on('-d', '--delay [NUM]', Integer, "Delay In Milliseconds") do |n|
     options[:delay] = n
   end
-  opts.on('-s', '--speed NUM', Integer, "Connection Speed in MBit/s") do |n|
+  opts.on('-s', '--speed [NUM]', Integer, "Connection Speed in MBit/s") do |n|
     options[:speed] = n
+  end
+  opts.on('-p', '--problem', "Problem?") do |n|
+    options[:problem] = n;
   end
   opts.on('-h', '--help', "Show this screen") do
     puts optparse; exit
   end
 end
 
-optparse.parse!(ARGV)
-
-if ARGV.length >= 4 || ARGV[0] == "-i" || ARGV[0] == "--interactive"
+if ARGV.length == 0
   puts optparse; exit
 end
+
+optparse.parse!(ARGV)
 
 if options[:interactive]
   print "Looks like you're trying to foul up your network, ya? (y/n): "
@@ -89,7 +121,7 @@ if options[:interactive]
       exit
     end
   else
-    print "What's the IP address of the end point? "
+    print "What's the IP/host of the end point? "
     endpoint = gets.chomp()
     print "How much delay in ms do you want? "
     delay = gets.chomp()
@@ -101,5 +133,9 @@ if options[:interactive]
     puts "Huzzah, things should be thoroughly messed up now, congrats!"
   end
 else
+  if options[:problem]
+    problem?
+    exit
+  end
   create_rules(options[:endpoint], options[:delay], options[:packetloss], options[:speed])
 end
--- README.md
+++ README.md
@@ -5,30 +5,33 @@ This is a script that came out of the need to make the iPhone simulator perform 
 
 Prerequisites
 -------------
- * OS X 10.6+ (I've only tested this on OS X, but should work with anything with ipfw installed)
+ * OS X 10.6+ (I've only tested this on OS X, but should work on anything with ipfw installed)
  * Ruby (duh)
  
 Usage
 -----
- >example: ruby Cholesterol.rb -e 10.0.0.1 -s 10 -p 57 -d 250
+ >example: ruby Cholesterol.rb -e 10.0.0.1 -s 10 -l 57 -d 250
 
- * -e specifies the endpoint you wish you limit your connection with
- * -s specifies the speed at which you wish to limit your connection
- * -p specifies the percentage of packet loss you wish to occur on the connection
- * -d specifies the amount of delay you wish to occur on the connection
+ * -e specifies the endpoint(s)* you wish you limit your connection with
+ * -s specifies the speed at which you wish to limit your connection (default is 100Mbit)
+ * -l specifies the percentage of packet loss you wish to occur on the connection (default is 0%)
+ * -d specifies the amount of delay you wish to occur on the connection (default is 0ms)
+
+ *make sure to enclose multiple endpoints in quotes if using a space between the comma delimiter.
 
 Additionally, you can use the -h or --help switch to receive the entire help output as such, 
->Usage: Cholesterol.rb -e endpoint -s speed -p packetloss -d delay
-
->   
-	-v, --version                    Display Version
+>Usage: Cholesterol.rb -e endpoint -s speed -l packetloss -d delay
+>
+    -v, --version                    Display Version
     -i, --interactive                I'll guide you through setup!
-    -p, --packet-loss (0-100)        Packet Loss Percentage
-    -e, --endpoint (i.e. 10.0.0.1)   Endpoint You Wish To Slow Up
+    -l, --packet-loss [0-100]        Packet Loss Percentage
+    -e, --endpoint [IP or Hostname]  Endpoint(s) To Slow Connectivity To (i.e. "www.google.com, www.wikipedia.org")
     -f, --flush                      Flush Old Rules
-    -d, --delay NUM                  Delay In Milliseconds
-    -s, --speed NUM                  Connection Speed in MBit/s
+    -d, --delay [NUM]                Delay In Milliseconds
+    -s, --speed [NUM]                Connection Speed in MBit/s
+    -p, --problem                    Problem?
     -h, --help                       Show this screen
+
 
 Planned Features
 ----------------
@@ -40,5 +43,6 @@ Contributing
 
 Changes
 -------
-
+* 11/26/2010 - Added support for multiple host addition. i.e. "www.google.com, www.wikipedia.org" will make both sets of rules.
+* 11/26/2010 - Added support if user enters no endpoint. This will cause everything to be clogged, also set default values if the 		user does not configure the script themselves. Added experimental -p option. 
 * 10/27/2010 - Added in hostname resolution and rule setting for all resolved IPs
